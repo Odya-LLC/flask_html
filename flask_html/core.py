eec7d1cc879ef9b4e778a066cecf0c58f2f0fe43
Odilxon <mmuhtor@gmail.com>
2022-11-10 17:54:49 +0500

    refactoring Item

--- flask_html/core.py
+++ flask_html/core.py
@@ -31,36 +31,31 @@ class Style:
 
     def render(self):
         return self.style
-
+    
 class Item:
     
     item  = "<{tag} {classes} {id} {props}>{content}</{tag}>"
-    __tag = None
-    __classes = None
-    __id = None
-    __props = None
+    
     page = None
-    elements = []
-    styles = None
-    js = ""
+    __classes = []
+    __id = None
+    __styles = []
+    __tag = None
+    __elements = []
+    __props = {}
+    __js = None
+    
     def register_style(self):
-        if self.styles:
-            self.page.register_style(self.styles[0], self.styles[1])
-        if self.js:
-            self.page.register_js(self.js)
-        for i in range(len(self.elements)):
-            if isinstance(self.elements[i], Item):
-                self.elements[i].page = self.page
-                self.elements[i].register_style()
-    
-    
-    def get_tag(self):
-        return self.__tag
+        if self.__styles:
+            self.page.register_style(self.__styles[0], self.__styles[1])
+        if self.__js:
+            self.page.register_js(self.__js)
+        for i in range(len(self.__elements)):
+            if isinstance(self.__elements[i], Item):
+                self.__elements[i].page = self.page
+                self.__elements[i].register_style()
     
     def __init__(self, page: Page = None, classes: List[str] = [], id: str = None, style: Style = None, tag: str = "div", content: List[object] = [], props: Dict[str, str] = {}):
-
-        classes = [] if len(classes) == 0 else classes
-        props = {} if len(props) == 0 else props
         """Base template for all HTML elements
 
         Args:
@@ -71,67 +66,59 @@ class Item:
             tag (str, optional): Tag name. Defaults to "div".
             content (List[object], optional): List of Elements. Defaults to [].
             props (Dict[str, str], optional): Tag rpoperties. Defaults to {}.
-        """        
+        """   
+        
+        classes = [] if len(classes) == 0 else classes
+        props = {} if len(props) == 0 else props
+        
         if page:
             self.page = page
-        self.elements = content
-        self._cls = classes
-        _cl = None
+        self.__elements = content
+        self.__classes = classes
         if style:
             _cl = self.__generate_style(style)
-            self._cls.append(_cl)
-        
-        if len(self._cls) > 0:
-            self.__append_classes()
-        else:
-            self.__classes = ""
+            self.__classes.append(_cl)
         if id:
-            self.__id = "id='{}'".format(id)
-        else:
-            self.__id = ""
+            self.__id = id
         self.__tag = tag
-        if len(props) > 0:
-            _props = ""
-            for key, value in props.items():
-                _props += "{}='{}' ".format(key, value)
-            self.__props = _props.rstrip(" ")
-        else:
-            self.__props = ""
+        self.__props = props
         
     def __str__(self):
         return self.render()
     
-    def __repr__(self):
-        return self.render()
-    
     def render(self):
-        """Render HTML element
-
-        Returns:
-            str: String HTML element
-        """        
+        _id = ""
+        if self.__id:
+            _id = "id='{}'".format(self.__id)
+        _classes = ""
+        if len(self.__classes) > 0:
+            _classes = "class='"
+            for _class in self.__classes:
+                _classes += _class + " "
+            _classes = _classes.rstrip(" ")
+            _classes = _classes + "'"
+        _props = ""
+        if len(self.__props) > 0:
+            for key, value in self.__props.items():
+                _props += "{}='{}' ".format(key, value)
+            _props = _props.rstrip(" ")
         _cnt = ""
-        for item in self.elements:
+        for item in self.__elements:
             _cnt += str(item)
-        return self.item.format(tag=self.__tag, classes=self.__classes, id=self.__id, content=_cnt, props=self.__props)
-
-    def __append_classes(self):
-        classes = self._cls
+        return self.item.format(tag=self.__tag, classes=_classes, id=_id, content=_cnt, props=_props)
         
-        _res = ""
-        if classes:
-            _res = "class='"
-            for _class in classes:
-                _res += _class + " "
-            _res = _res.rstrip(" ")
-            _res = _res + "'"
-        self.__classes = _res
-        
+    
     def __generate_style(self, style: Style):
         styles = style.render()
         self.hash_code = "o" + sha256("{secret}{styles}".format(secret=current_app.config.get("SECRET_KEY", "123123"), styles=styles).encode()).hexdigest()[:5]
         return self.__register_style(self.hash_code, styles)
 
     def __register_style(self, hash_code: str, styles: str):
-        self.styles = (hash_code, styles)
+        self.__styles = (hash_code, styles)
         return hash_code
+    
+    def on(self, event, func):
+        if not self.__id:
+            self.__id = "o" + sha256("{secret}{tag}{event}{func}".format(secret=current_app.config.get("SECRET_KEY", "123123"), tag=str(self.__tag), event=event, func=func).encode()).hexdigest()[:5]
+        self.__js = "document.getElementById('{}').addEventListener('{}', function() {{ {} }});".format(self.__id, event, func)
+        return self
